5caee43b47b098a48beb256afabd590987ad2386
alexkallai <alexkallai92@gmail.com>
2022-12-26 22:42:41 +0100

    Added multi file suport during opening

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import file
 from tkinter import filedialog as fd
 
-
 if __name__ == "__main__":
-    filename = fd.askopenfilename()
-    with open(filename, "rb") as f:
-        opened_file = file.File(f.read())
-    opened_file.plot_2D_digraph()
+    filenames = fd.askopenfilenames()
+    for filename in filenames:
+        with open(filename, "rb") as f:
+            opened_file = file.File(f.read())
+            opened_file.plot_2D_digraph()
