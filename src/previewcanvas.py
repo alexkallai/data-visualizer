08c8c29bf81e73ae3f0652204ed3c5dc7e25c862
alexkallai <alexkallai92@gmail.com>
2023-01-22 10:53:36 +0100

    Added refit function

--- src/previewcanvas.py
+++ src/previewcanvas.py
@@ -5,13 +5,15 @@ import numpy as np
 class PreviewCanvas:
     def __init__(self):
         self.canvas = SceneCanvas()
+        self.x_range = 100
+        self.y_range = 100
         #self.canvas.size = (200, 0)
         self.grid = self.canvas.central_widget.add_grid()
 
         #self.view_top = self.grid.add_view(0, 0, bgcolor='black')
         #self.view_top = self.grid.add_view(row_span=200, col_span=200, bgcolor='black')
         self.view_top = self.grid.add_view( bgcolor='black')
-        image_data = np.empty( (100, 100), dtype=np.uint8 )
+        image_data = np.empty( (self.x_range, self.y_range), dtype=np.uint8 )
         self.image: visuals.Image = visuals.Image(
             data=image_data,
             texture_format="auto",
@@ -26,9 +28,15 @@ class PreviewCanvas:
         print(f"Changing image colormap to {cmap_name}")
         self.image.cmap = cmap_name
     
-    def set_image(self, image: np.ndarray):
+    def set_image(self, image: np.ndarray) -> None:
         self.image.set_data(image)
-        self.view_top.camera.set_range(x=(0, image.shape[1]), y=(0, image.shape[0]), margin=0)
+        self.x_range = image.shape[1]
+        self.y_range = image.shape[0]
+        self.view_top.camera.set_range(x=(0, self.x_range), y=(0, self.y_range), margin=0)
         #self.image._update_colortransform_clim()
         #self.image.update()
+        self.view_top.update()
+    
+    def refit_image(self) -> None:
+        self.view_top.camera.set_range(x=(0, self.x_range), y=(0, self.y_range), margin=0)
         self.view_top.update()
