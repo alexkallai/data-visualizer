6b808b7fe6aa2780fa0d2c606ff7112611e3f4b6
alexkallai <scythe1092@gmail.com>
2023-08-09 21:28:21 +0200

    Plotwidget refactoring

--- src/plotwidget.py
+++ src/plotwidget.py
@@ -6,6 +6,7 @@ import numpy as np
 class PlotCanvasWrapper:
     def __init__(self):
         self.plotcanvas = Fig(bgcolor="#F0F0F0")
+        self.plotwidget = self.plotcanvas[0, 0]
         self.color = (0.3, 0.5, 0.8)
 
     def set_image_colormap(self, cmap_name: str):
@@ -13,8 +14,8 @@ class PlotCanvasWrapper:
 
     def set_plot(self, plot_data: np.ndarray) -> None:
         # plot_data: 1D array
-        self.plotcanvas[0:4, 0:4].histogram(plot_data, bins=256, color=self.color, orientation='h')
-        self.plotcanvas.update()
+        self.plotwidget.histogram(plot_data, bins=256, color=self.color, orientation='h')
+        #self.plotwidget.update()
 
     def refit_image(self) -> None:
         pass
