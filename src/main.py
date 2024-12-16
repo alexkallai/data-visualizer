799438a9d8b4278732b4c5168189d0ded9486aaa
alexkallai <scythe1092@gmail.com>
2024-12-16 19:13:02 +0100

    Basic DPG window

--- src/main.py
+++ src/main.py
@@ -1,12 +1,44 @@
-from PyQt5.QtWidgets import QApplication
-from main_window import MainWindow
-import sys
+import dearpygui.dearpygui as dpg
 
-def main():
-    application = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(application.exec_())
+STATUS_BAR_HEIGHT = 20
+
+def build_window():
+    with dpg.theme() as status_bar_theme:
+        with dpg.theme_component():
+            dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (42, 123, 207, 255))
+            dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 4, 0)
+
+    def resize_primary_window():
+        x,y = dpg.get_item_rect_size(primary_window)
+        dpg.configure_item(status_bar, width=x)
+        dpg.configure_item(status_bar, pos=(0, y-STATUS_BAR_HEIGHT))
+
+    with dpg.window() as primary_window:
+        dpg.set_primary_window(primary_window, True)
+        with dpg.item_handler_registry() as registry:
+            dpg.add_item_resize_handler(callback=resize_primary_window)
+        dpg.bind_item_handler_registry(primary_window, registry)
+
+        with dpg.menu_bar():
+            with dpg.menu(label="View"):
+                dpg.add_menu_item(label="Show/hide status bar", callback=lambda: dpg.configure_item(status_bar, show=not dpg.is_item_shown(status_bar)))
+
+        with dpg.window():
+            dpg.add_button(label="Hello world")
+
+        with dpg.window(no_title_bar=True, no_move=True, no_resize=False) as status_bar:
+            dpg.bind_item_theme(status_bar, status_bar_theme)
+            with dpg.group(horizontal=True):
+                dpg.add_text("Hello")
+                dpg.add_button(label="world")
 
 if __name__ == "__main__":
-    main()
+    dpg.create_context()
+
+    build_window()
+
+    dpg.create_viewport(width=800, height=600)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
