2db32d23cdb7c7ffe278a58a636006440895e5de
alexkallai <scythe1092@gmail.com>
2025-02-04 22:49:48 +0100

    Also removed qt dep

--- src/util.py
+++ src/util.py
@@ -1,15 +1,14 @@
-from PyQt5.QtWidgets import QFileDialog
 import os
 from time import time
 
 
 # Return the file name
-def get_file_name():
-    return QFileDialog.getOpenFileName(
-        parent=None,
-        caption="Open a file",
-        directory=os.getcwd()
-    )
+#def get_file_name():
+    #return QFileDialog.getOpenFileName(
+        #parent=None,
+        #caption="Open a file",
+        #directory=os.getcwd()
+    #)
 
 def timer(func):
     # This function shows the execution time of 
